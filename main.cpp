22c87072fe2f5d1645eda4289f22ba00c5addc41
CHIINGTING <tad20077@gmail.com>
2021-12-21 23:32:31 +0800

    modify

--- main.cpp
+++ main.cpp
@@ -240,7 +240,7 @@ auto funB = [](int id, int maxNum) -> void{
         cout<< "philosopher Num: "<< id+1 << " done eat: "<< eat <<" thread id = "<<this_thread::get_id()<<" is releasing left chopstick"<<endl;
         this_thread::sleep_for(chrono::seconds(random()%5));
         alock.unlock();
-        sem.notify_all();
+      //  sem.notify_all();
     };
     auto releaseRightChopstick = [&]() -> void {
         chopsticks[(id) % maxNum].signal();
@@ -248,14 +248,14 @@ auto funB = [](int id, int maxNum) -> void{
         cout<< "philosopher Num: "<< id+1 << " done eat: "<< eat <<" thread id = "<<this_thread::get_id()<<" is releasing left chopstick"<<endl;
         this_thread::sleep_for(chrono::seconds(random()%5));
         alock.unlock();
-        sem.notify_all();
+       // sem.notify_all();
     };
     auto thinking = [=]()-> void {
         alock.lock();
         cout<< "philosopher Num: "<< id+1 <<" thread id = "<<this_thread::get_id()<<" is thinking"<<endl;
         std::this_thread::sleep_for(chrono::seconds(random()%5+5));
         alock.unlock();
-        sem.notify_all();
+        //sem.notify_all();
     };
     // the philosopher join meal Number
 
