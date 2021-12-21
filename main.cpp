f064b0e06964df60d6431df0d45c3da8c5adcd57
CHIINGTING <tad20077@gmail.com>
2021-12-21 23:36:41 +0800

    modify

--- main.cpp
+++ main.cpp
@@ -310,7 +310,7 @@ auto funC = [](int id, int maxNum) -> void {
         cout<< "philosopher Num: "<< id+1 <<" thread id = "<<this_thread::get_id()<<" is thinking"<<endl;
         std::this_thread::sleep_for(chrono::seconds(random()%5+5));
         alock.unlock();
-        sem.notify_all();
+       // sem.notify_all();
     };
     while (eat<3){
         eating();
