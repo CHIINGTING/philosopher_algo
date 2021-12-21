dfe7d0f2874528e3295cddb61d6d7a9bdade094e
CHIINGTING <tad20077@gmail.com>
2021-12-22 00:03:29 +0800

    modify

--- main.cpp
+++ main.cpp
@@ -307,10 +307,11 @@ auto funC = [](int id, int maxNum) -> void {
         eat++;
         std::this_thread::sleep_for(chrono::seconds(random()%5));
 
-        chopsticks[id]=0;
-        chopsticks[(id + 1) % maxNum].signal();
+        chopsticks[id]=1;
+        chopsticks[(id + 1) % maxNum]=1;
         cout<< "philosopher Num: "<< id+1 <<" thread id = "<<this_thread::get_id()<<" done eating and he eat: " << eat << " times." <<endl;
         alock.unlock();
+        sem.notify_all();
     };
     auto thinking = [&]()->void{
         alock.lock();
