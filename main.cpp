863a6f7fa1a7839704ee78d8ff6325ae77114634
CHIINGTING <tad20077@gmail.com>
2021-12-21 11:23:01 +0800

    modify

--- main.cpp
+++ main.cpp
@@ -123,13 +123,14 @@ public:
 
 // algo
 auto funA = [](int id, int maxNum) -> void{
+    std::condition_variable matex;
     int eat=0;
     auto eating = [&]() -> void{
 
-        unique_lock<mutex> locker(alock);
+        unique_lock<mutex> funaLock(alock);
         while (chopsticks[id]==0){
             cout<< "philosopher Num: "<< id << " done eat :"<< eat+1 <<" thread id = "<<this_thread::get_id()<<"is waiting"<<endl;
-            sem.wait(locker);
+            matex.wait(funaLock);
         }
         cout<< "in eating"<<endl;
         cout<< "chopsticks["<<id<<"].wait();"<<endl;
@@ -138,6 +139,7 @@ auto funA = [](int id, int maxNum) -> void{
         this_thread::sleep_for(chrono::seconds(random()%5));
         chopsticks[id].up();
         eat++;
+        matex.notify_all();
     };
     auto thinking = [=]() -> void{
         cout<< "philosopher Num: "<< id << " done eat :"<< eat+1 <<" thread id = "<<this_thread::get_id()<<"is thinking"<<endl;
