692e6119295b1b5faf9a5ce7d14c9dc46e6224f4
CHIINGTING <tad20077@gmail.com>
2021-12-21 23:44:20 +0800

    modify

--- main.cpp
+++ main.cpp
@@ -73,8 +73,8 @@ public:
         alock.lock();
       //  cout<<"in singal lock"<<endl;
         m++;
-        sem.notify_all();
         alock.unlock();
+        sem.notify_all();
     }
     void signal(int id) {
        // cout<<"in singal"<<endl;
@@ -287,6 +287,11 @@ auto funC = [](int id, int maxNum) -> void {
     int eat = 0;
     auto eating = [&]() -> void {
         //
+        while (chopsticks[id] == 0 || chopsticks[(id + 1) % maxNum] == 0){
+            unique_lock<mutex> locker(alock);
+            cout << "philosopher Num: " << id+1 << " done eat :" << eat << " thread id = " << this_thread::get_id() << " is waiting" << endl;
+            sem.wait(locker);
+        }
         chopsticks[id].wait(id);
         chopsticks[(id + 1) % maxNum].wait();
         alock.lock();
